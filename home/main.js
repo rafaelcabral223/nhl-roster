ff41647922d13001731bf555f8fec68ffa8be0fa
Rafael <rafaelcpribeiro@gmail.com>
2023-09-18 22:12:20 -0300

    'aaa'

--- home/main.js
+++ home/main.js
@@ -9,35 +9,44 @@ fetch('https://statsapi.web.nhl.com/api/v1/teams/19?expand=team.roster').then(re
         const datas = data.teams[0].roster.roster
         
 
-         const players = datas.map((user, index, array) => {
-            const getPersonalLink = `https://statsapi.web.nhl.com/api/v1/people/${user.person.id}`
-            
-             const html = `
-                 <div class="card">
-                     <div class="player">
-                         <img class="player-img" src="https://assets.nhle.com/mugs/nhl/20222023/STL/${user.person.id}.png">
-                         <p class="player-info person-fullname">Player: ${user.person.fullName}</p>
-                         <p class="player-info person-jerseynumber">#${user.jerseyNumber}</p>
-                         <p class="player-info person-position-code">Pos: ${user.position.code}</p>
-                         <!-- <p class="player-info person-id">${getPersonalLink}</p> -->
-                     </div>
-                 </div>
-             `
+            datas.forEach(user => {
+                const getPersonalLink = `https://statsapi.web.nhl.com/api/v1/people/${user.person.id}`;
+
+                const html = `
+                    <div class="card ${user.position.type}">
+                        <div class="player">
+                            <img class="player-img" src="https://assets.nhle.com/mugs/nhl/20222023/STL/${user.person.id}.png">
+                            <p class="player-info person-fullname">Player: ${user.person.fullName}</p>
+                            <p class="player-info person-jerseynumber">#${user.jerseyNumber}</p>
+                            <p class="player-info person-position-code">Pos: ${user.position.code}</p>
+                            <!-- <p class="player-info person-id">${getPersonalLink}</p> -->
+                        </div>
+                    </div>
+                `
 
             if (user.position.type === "Goalie") {
-                document.getElementById('goaliePlayers').innerHTML += html
+                document.getElementById('goaliePlayers').innerHTML += 
+                `
+                        ${html}
+                        <!-- Aqui serão exibidos os jogadores Goalie -->
+                `
             }
 
             if (user.position.type === "Defenseman") {
-                document.getElementById('defensePlayers').innerHTML += html 
-                console.log(user)
+                console.log('oi')
             }
                 
             if (user.position.type === "Forward") {
-                document.getElementById('forwardPlayers').innerHTML += html 
+                console.log('oi')
             }
         })
             
        console.log(datas)
     
 })
+
+
+
+
+
+
