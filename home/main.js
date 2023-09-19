959e4b5bb1172b061318f377f435e40ce81d1758
Rafael <rafaelcpribeiro@gmail.com>
2023-09-18 21:29:08 -0300

    incompleto

--- home/main.js
+++ home/main.js
@@ -12,17 +12,17 @@ fetch('https://statsapi.web.nhl.com/api/v1/teams/19?expand=team.roster').then(re
          const players = datas.map((user, index, array) => {
             const getPersonalLink = `https://statsapi.web.nhl.com/api/v1/people/${user.person.id}`
             
-            const html = `
-                <div class="card">
-                    <div class="player">
-                        <img class="player-img" src="https://assets.nhle.com/mugs/nhl/20222023/STL/${user.person.id}.png">
-                        <p class="player-info person-fullname">Player: ${user.person.fullName}</p>
-                        <p class="player-info person-jerseynumber">#${user.jerseyNumber}</p>
-                        <p class="player-info person-position-code">Pos: ${user.position.code}</p>
-                        <!-- <p class="player-info person-id">${getPersonalLink}</p> -->
-                    </div>
-                </div>
-            `
+             const html = `
+                 <div class="card">
+                     <div class="player">
+                         <img class="player-img" src="https://assets.nhle.com/mugs/nhl/20222023/STL/${user.person.id}.png">
+                         <p class="player-info person-fullname">Player: ${user.person.fullName}</p>
+                         <p class="player-info person-jerseynumber">#${user.jerseyNumber}</p>
+                         <p class="player-info person-position-code">Pos: ${user.position.code}</p>
+                         <!-- <p class="player-info person-id">${getPersonalLink}</p> -->
+                     </div>
+                 </div>
+             `
 
             if (user.position.type === "Goalie") {
                 document.getElementById('goaliePlayers').innerHTML += html
@@ -34,12 +34,10 @@ fetch('https://statsapi.web.nhl.com/api/v1/teams/19?expand=team.roster').then(re
             }
                 
             if (user.position.type === "Forward") {
-                //document.getElementById('forwardPlayers').innerHTML += html 
+                document.getElementById('forwardPlayers').innerHTML += html 
             }
         })
             
-
-
        console.log(datas)
     
 })
